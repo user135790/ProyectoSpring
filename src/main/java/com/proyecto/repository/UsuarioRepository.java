88661f1fd969477e6f9f273448cab93ae40f412f
user135790 <50127993+user135790@users.noreply.github.com>
2023-02-22 11:51:13 -0500

    Creacion de repositorio de usuarios

--- src/main/java/com/proyecto/repository/UsuarioRepository.java
+++ src/main/java/com/proyecto/repository/UsuarioRepository.java
@@ -1,5 +1,15 @@
 package com.proyecto.repository;
 
-public interface UsuarioRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.proyecto.enums.PerfilUsuario;
+import com.proyecto.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+	List<Usuario> findByPerfil(PerfilUsuario perfil);
 }
