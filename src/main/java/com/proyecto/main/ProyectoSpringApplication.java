4840aac3fdf3dce705d289476d0ccb7dc3d7b4a9
user135790 <50127993+user135790@users.noreply.github.com>
2023-02-24 09:36:49 -0500

    Creando usuario por defecto

--- src/main/java/com/proyecto/main/ProyectoSpringApplication.java
+++ src/main/java/com/proyecto/main/ProyectoSpringApplication.java
@@ -1,15 +1,31 @@
 package com.proyecto.main;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+
+import com.proyecto.main.enums.PerfilUsuario;
+import com.proyecto.main.model.Usuario;
+import com.proyecto.main.repository.UsuarioRepository;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.proyecto.main.repository")
 public class ProyectoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner demo(UsuarioRepository repository) {
+		return (args) -> {
+			// save a few customers
+		Usuario usuario = new Usuario();
+		usuario.setNombre("admin");
+		usuario.setContrasena("Admin123");
+		usuario.setCorreoElectronico("admin@gmail.com");
+		usuario.setPerfil(PerfilUsuario.ADMINISTRADOR);
+		repository.save(usuario);
+		};
+	}
 }
